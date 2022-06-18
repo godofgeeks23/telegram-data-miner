476389baa370f58881a04f88c681c006eb25358e
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-06-18 06:32:00 +0530

    Create auth_test.py

---
+++ auth_test.py
@@ -0,0 +1,31 @@
+import configparser
+import json
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+
+
+# Reading Configs
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Setting configuration values
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+
+api_hash = str(api_hash)
+
+phone = config['Telegram']['phone']
+username = config['Telegram']['username']
+
+# Create the client and connect
+client = TelegramClient(username, api_id, api_hash)
+client.start()
+print("Client Created")
+# Ensure you're authorized
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    try:
+        client.sign_in(phone, input('Enter the code: '))
+    except SessionPasswordNeededError:
+        client.sign_in(password=input('Password: '))
+
