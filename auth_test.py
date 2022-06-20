3da321adcadbe70510ccb99208ddeb4bb6e7f3c4
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-06-20 07:02:55 +0530

    Update auth_test.py

--- auth_test.py
+++ auth_test.py
@@ -8,6 +8,10 @@ from telethon.tl.types import (
 PeerChannel
 )
 import telethon.sync
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+PeerChannel
+)
 
 # Reading Configs
 config = configparser.ConfigParser()
@@ -35,33 +39,28 @@ if not client.is_user_authorized():
 
 # getting channel members info
 user_input_channel = input("enter entity(telegram URL or entity id):")
-
 if user_input_channel.isdigit():
     entity = PeerChannel(int(user_input_channel))
 else:
     entity = user_input_channel
-
 my_channel = client.get_entity(entity)
 offset = 0
 limit = 100
 all_participants = []
-
 while True:
     participants = client(GetParticipantsRequest(
         my_channel, ChannelParticipantsSearch(''), offset, limit,
         hash=0
     ))
-    
     if not participants.users:
         break
     all_participants.extend(participants.users)
     offset += len(participants.users)
-
 all_user_details = []
 for participant in all_participants:
     all_user_details.append(
         {"id": participant.id, "first_name": participant.first_name, "last_name": participant.last_name,
          "user": participant.username, "phone": participant.phone, "is_bot": participant.bot})
-
-with open('user_data.json', 'w') as outfile:
-    json.dump(all_user_details, outfile)
+# with open('user_data.json', 'w') as outfile:
+#     json.dump(all_user_details, outfile)
+print(all_user_details)
