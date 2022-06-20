5600129ef94927a050689b171c3b7c85e96c30d4
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-06-20 07:07:33 +0530

    Update auth_test.py

--- auth_test.py
+++ auth_test.py
@@ -13,10 +13,13 @@ from telethon.tl.types import (
 PeerChannel
 )
 
+# ---------------------------------------------------------------------------------------------------------
 # Reading Configs
 config = configparser.ConfigParser()
 config.read("config.ini")
+# ---------------------------------------------------------------------------------------------------------
 
+# ---------------------------------------------------------------------------------------------------------
 # Setting configuration values
 api_id = config['Telegram']['api_id']
 api_hash = config['Telegram']['api_hash']
@@ -24,7 +27,9 @@ api_hash = str(api_hash)
 phone = config['Telegram']['phone']
 username = config['Telegram']['username']
 passwd = config['Telegram']['password']
+# ---------------------------------------------------------------------------------------------------------
 
+# ---------------------------------------------------------------------------------------------------------
 # Create the client and connect
 client = TelegramClient(username, api_id, api_hash)
 client.start()
@@ -36,7 +41,9 @@ if not client.is_user_authorized():
         client.sign_in(phone, input('Enter the code: '))
     except SessionPasswordNeededError:
         client.sign_in(password=passwd)
+# ---------------------------------------------------------------------------------------------------------
 
+# ---------------------------------------------------------------------------------------------------------
 # getting channel members info
 user_input_channel = input("enter entity(telegram URL or entity id):")
 if user_input_channel.isdigit():
@@ -63,4 +70,35 @@ for participant in all_participants:
          "user": participant.username, "phone": participant.phone, "is_bot": participant.bot})
 # with open('user_data.json', 'w') as outfile:
 #     json.dump(all_user_details, outfile)
-print(all_user_details)
+# print(all_user_details)
+# ---------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------
+offset_id = 0
+limit = 100
+all_messages = []
+total_messages = 0
+total_count_limit = 0
+
+while True:
+    print("Current Offset ID is:", offset_id, "; Total Messages:", total_messages)
+    history = client(GetHistoryRequest(
+        peer=my_channel,
+        offset_id=offset_id,
+        offset_date=None,
+        add_offset=0,
+        limit=limit,
+        max_id=0,
+        min_id=0,
+        hash=0
+    ))
+    if not history.messages:
+        break
+    messages = history.messages
+    for message in messages:
+        all_messages.append(message.to_dict())
+    offset_id = messages[len(messages) - 1].id
+    total_messages = len(all_messages)
+    if total_count_limit != 0 and total_messages >= total_count_limit:
+        break
+# ---------------------------------------------------------------------------------------------------------
