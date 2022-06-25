ac292a942137a9368c3f4abb2c9fbf3d86e0cf2f
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-06-25 09:09:08 +0530

    Update auth_test_beta.py

--- auth_test_beta.py
+++ auth_test_beta.py
@@ -5,12 +5,12 @@ from telethon.errors import SessionPasswordNeededError
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsSearch
 from telethon.tl.types import (
-PeerChannel
+    PeerChannel
 )
 import telethon.sync
 from telethon.tl.functions.messages import (GetHistoryRequest)
 from telethon.tl.types import (
-PeerChannel
+    PeerChannel
 )
 from telethon.sync import TelegramClient
 from telethon import functions, types
@@ -122,11 +122,12 @@ if not client.is_user_authorized():
 
 # ---------------------------------------------------------------------------------------------------------
 # Searching channels
+
+
 def search_channels():
     results = []
-    search_keywords = ["drugs"
-    ,"marijuana","cocaine","tylenol","Oxycodone","Ritalin","Adderall ","Oxycotine","Diazepam Valium","Xanax","Dilaudid","Hydrocodone ","Percocet ","MDMA ","Tramadol","Suboxone","Rohypnol","Tilidin","Concerta","Morphine","Opana","Oxynorm","Mandrax","Quaalude","Codeine","LortabWatson","Dilaudid ","Fentanyl","Stilnox ","viagra","Subutex","lyrica","vicodine5","seconal","Dexedrine","Morphine","Xanax","Diazepam","Methadone","Oxycodone","Heroin","Percocet","Subutex","Hydrocodone","Methadone","Lyrica","Adderall","Diazepam","Roxicodone","Rohypnol","Vicodin","Benzodiazepines","Clozapine","Colchicine","smuggle","smuggling","drugs smuggling","drugs trafficking","escort services","escorts","call girl","escorts india","darkweb","dark web","onion","onion sites","dark web links","malware","ransomware","carding","criminal","deep web","deepweb","onion links","malware mods","child porn","porn onion","dark web porn","child porn onion","guns","buy guns","dark web guns"
-    ]
+    search_keywords = ["drugs", "marijuana", "cocaine", "tylenol", "Oxycodone", "Ritalin", "Adderall ", "Oxycotine", "Diazepam Valium", "Xanax", "Dilaudid", "Hydrocodone ", "Percocet ", "MDMA ", "Tramadol", "Suboxone", "Rohypnol", "Tilidin", "Concerta", "Morphine", "Opana", "Oxynorm", "Mandrax", "Quaalude", "Codeine", "LortabWatson", "Dilaudid ", "Fentanyl", "Stilnox ", "viagra", "Subutex", "lyrica", "vicodine5", "seconal", "Dexedrine", "Morphine", "Xanax", "Diazepam", "Methadone", "Oxycodone", "Heroin", "Percocet", "Subutex", "Hydrocodone", "Methadone", "Lyrica", "Adderall", "Diazepam", "Roxicodone", "Rohypnol", "Vicodin", "Benzodiazepines", "Clozapine", "Colchicine", "smuggle", "smuggling", "drugs smuggling", "drugs trafficking", "escort services", "escorts", "call girl", "escorts india", "darkweb", "dark web", "onion", "onion sites", "dark web links", "malware", "ransomware", "carding", "criminal", "deep web", "deepweb", "onion links", "malware mods", "child porn", "porn onion", "dark web porn", "child porn onion", "guns", "buy guns", "dark web guns", "india drug", "indian drugs", "ganja", "marijuana", "buy marijuana", "marijuana market", "marijuana east", "afeem", "afeem india", "afeem delhi", "charas", "charas india", "hashish", "hashish india", "hashish buy"
+                       ]
     for search in search_keywords:
         result = client(functions.contacts.SearchRequest(
             q=search,
@@ -138,7 +139,7 @@ def search_channels():
             print(f"t.me/{section.username}")
         for section in result.users:
             print(f"t.me/{section.username}")
-        
+
             # print()
         # print(len(result.my_results) + len(result.results) + len(result.chats) + len(result.users), f"Results found for query: {search}")
         # print()
@@ -146,9 +147,11 @@ def search_channels():
         results.append(result)
     total_results_count = 0
     for result in results:
-        total_results_count+= len(result.my_results) + len(result.results) + len(result.chats) + len(result.users)
+        total_results_count += len(result.my_results) + \
+            len(result.results) + len(result.chats) + len(result.users)
 
     print(f"Total: {total_results_count}")
+
 
 # ---------------------------------------------------------------------------------------------------------
 search_channels()
