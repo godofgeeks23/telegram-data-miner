b975a8f1df02bec4aee7588f21eb6ea0b3491a53
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-06-27 23:58:40 +0530

    Update nlp_v2.py

--- nlp_v2.py
+++ nlp_v2.py
@@ -21,17 +21,14 @@ filtered_list = []
 for word in words:
     if word.casefold() not in stop_words:
         filtered_list.append(word)
-try:
-    while True:
-        filtered_list.remove('.')
-        filtered_list.remove(',')
-        filtered_list.remove("'s")
-except ValueError:
-    pass
 
 filtered_list = ([re.sub('[^a-zA-Z0-9]+', '', _) for _ in filtered_list])
+filtered_list = [element for element in filtered_list if any(digit not in element for digit in "1234567890")]
+filtered_list = [x for x in filtered_list if not any(x1.isdigit() for x1 in x)]
 filtered_list = list(filter(None, filtered_list))
 filtered_list = [x for x in filtered_list if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
+filtered_list = [x for x in words if len(x) > 2]
+filtered_list = [x for x in filtered_list if x.isalpha()]
 print("\nfiltered_list - ", filtered_list)
 
 
