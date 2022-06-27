cc098b0035332f536cf1c6d7ecef50bcc0b68408
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-06-27 23:13:19 +0530

    Create nlp_v2.py

---
+++ nlp_v2.py
@@ -0,0 +1,43 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+text = ''
+with open("txts/msgdump2.txt") as file_in:
+    text = file_in.read()
+
+# sentences = sent_tokenize(text)
+# print("\nsentences - ", sentences)
+
+words = word_tokenize(text)
+print("\nwords - ", words)
+
+stop_words = set(stopwords.words("english"))
+filtered_list = []
+for word in words:
+    if word.casefold() not in stop_words:
+        filtered_list.append(word)
+try:
+    while True:
+        filtered_list.remove('.')
+        filtered_list.remove(',')
+        filtered_list.remove("'s")
+except ValueError:
+    pass
+print("\nfiltered_list - ", filtered_list)
+
+# stemmer = PorterStemmer()
+stemmer = SnowballStemmer("english")
+stemmed_words = [stemmer.stem(word) for word in filtered_list]
+print("\nstemmed_words - ", stemmed_words)
+
+pos_tagged_words = nltk.pos_tag(filtered_list)
+print("\npos_tagged_words - ", pos_tagged_words)
+
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_list]
+print("\nlemmatized_words - ", lemmatized_words)
